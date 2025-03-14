8be1f3e6d48950007172dbb9a8e2a1dfb4c8e1c8
Nams93 <contactpro93200@gmail.com>
2025-03-14 14:30:39 +0100

    Ajout de la configuration Supabase et des variables d'environnement

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+mport { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = "https://lvpgdgfaiouimffupmjv.supabase.co";
+const supabaseAnonKey = 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cGdkZ2ZhaW91aW1mZnVwbWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzY4MzUsImV4cCI6MjA1NzQ1MjgzNX0.jtyX75OCzjWJ7T0KNltmp2nh1jrGXVuN-03k-e_7KMQ";
 
